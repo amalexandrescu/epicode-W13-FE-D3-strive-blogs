307e0294c5d402cb8c492661cc32d38a97020f37
Alexis <alexandrescu.alexandra.maria@gmail.com>
2022-12-21 19:01:29 +0200

    BE connected with FE for get method

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import posts from "../../../data/posts.json";
+// import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function getBlogPosts() {
+      try {
+        let req = await fetch("http://localhost:3001/blogPosts");
+        if (req.ok) {
+          let blogPosts = await req.json();
+          setPosts(blogPosts);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getBlogPosts();
+  }, []);
   return (
     <Row>
       {posts.map((post) => (
